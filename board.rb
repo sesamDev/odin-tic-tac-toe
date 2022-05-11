b4cdbdf1e425bdb842e2676b60ec879859a126d4
Sebastian <sebastian.samils@gmail.com>
2022-05-11 20:18:37 +0200

    Can now print an empty grid

--- board.rb
+++ board.rb
@@ -1,5 +1,24 @@
 class Board
   def initialize
-    #
+    @@grid = []
+    create_board()
+    show_board()
   end
+
+  def create_board
+    n = 1
+    3.times do |x|
+      row = []
+      3.times { |y| row.push(" ") }
+      @@grid.push(row)
+    end
+  end
+
+  def show_board
+    p @@grid[0]
+    p @@grid[1]
+    p @@grid[2]
+  end
+  
 end
+
